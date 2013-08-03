33034dae7002987de31cc30a850921a32527c0a0
sekunovsky <sekunovsky@gmail.com>
2013-08-03 13:22:27 -0700

    abstracted gameload

--- game/public/src/game.js
+++ game/public/src/game.js
@@ -1,100 +1,18 @@
 var socket = io.connect('http://localhost:8080');
 
-var sources = {
-  greendino: '/dino-green/dino-sprite.png',
-  chicken: '/chicken/chicken-sprite.png',
-  reddino: '/dino-red/dino-sprite.png',
-};
-var images;
-var loadImages = function(sources, callback) {
-  var assetDir = './sprites';
-  images = {};
-
-  var loadedImages = 0;
-  var numImages = 0;
-  for (var src in sources) {
-    numImages++;
-  }
-  for (src in sources) {
-    images[src] = new Image();
-    images[src].onload = function() {
-      if (++loadedImages >= numImages) {
-        callback(images);
-      }
-    };
-    images[src].src = assetDir + sources[src];
-  }
-};
-
-var chickens = [];
-var counter = 0;
-
-var changeCounter = function(){
-  counter++;
-  $('.chickenCounter').text('CHICKENS: ' + counter)
-  console.log($('.chickenCounter').text())
+var init = function() {
+  this.game = new Game();
 }
 
-var loadStage = function(images) {
-
-  var background = document.getElementById('background');
-
-  window.stage = new Kinetic.Stage({
-    container: 'game',
-    width: window.innerWidth,
-    height: window.innerHeight
-  });
-
-  var layer = new Kinetic.Layer();
-
-  // var chickenCounter = new Kinetic.Text({
-  //   x: stage.getWidth() / 2,
-  //   y: 25,
-  //   text: 'CHICKENS: 0',
-  //   fontSize: 20,
-  //   fontFamily: 'Calibri',
-  //   fill: 'white'
-  // });
-
-  // layer.add(chickenCounter);
-
-  GreenDino();
-  layer.add(GreenDino.greenDinoObj);
-
-  for (var i = 0; i < 30; i++) {
-    var randomX = Math.floor((Math.random()*2048)+1);
-    var randomY = Math.floor((Math.random()*2048)+1);
-    Chicken(randomX, randomY);
-    layer.add(Chicken.chickenObj);
-    chickens.push(Chicken.chickenObj)
-  }
-
-  stage.add(layer);
-
-  for (var i = 0; i < chickens.length; i++) {
-    chickens[i].start();
-  }
-
-  GreenDino.greenDinoObj.start();
-  keyBindings(GreenDino, GreenDino.greenDinoObj, 'up', 'left', 'right', 'space', '/');
-
-  gameLoop();
-
-};
-
-loadImages(sources, loadStage);
-
-var frames = 0;
 var gameLoop = function(time){
   frames++;
   // if (frames % 100 === 0) {
   //   console.log(frames / (time/1000));
   // }
-
   collisionHandler(GreenDino, chickens);
   Chicken.update(time);
   GreenDino.update(time);
   checkBoundaries();
   translateScreen(); 
-  requestAnimationFrame(gameLoop);
+  requestAnimationFrame(this.gameLoop);
 };
---
+++ game/public/src/gameload.js
@@ -0,0 +1,72 @@
+var chickens = [];
+var counter = 0;
+
+var Game = function() {
+  this.sources = {
+    greendino: '/dino-green/dino-sprite.png',
+    chicken: '/chicken/chicken-sprite.png',
+    reddino: '/dino-red/dino-sprite.png',
+  };
+  this.images;
+  
+  this.loadImages = function(sources, callback) {
+    var assetDir = './sprites';
+    images = {};
+
+    var loadedImages = 0;
+    var numImages = 0;
+    for (var src in sources) {
+      numImages++;
+    }
+    for (src in sources) {
+      images[src] = new Image();
+      images[src].onload = function() {
+        if (++loadedImages >= numImages) {
+          callback(images);
+        }
+      };
+      images[src].src = assetDir + sources[src];
+    }
+  };
+
+  this.loadStage = function(images) {
+
+    var background = document.getElementById('background');
+
+    window.stage = new Kinetic.Stage({
+      container: 'game',
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+
+    var layer = new Kinetic.Layer();
+
+    GreenDino();
+    layer.add(GreenDino.greenDinoObj);
+
+    for (var i = 0; i < 30; i++) {
+      var randomX = Math.floor((Math.random()*2048)+1);
+      var randomY = Math.floor((Math.random()*2048)+1);
+      Chicken(randomX, randomY);
+      layer.add(Chicken.chickenObj);
+      chickens.push(Chicken.chickenObj)
+    }
+
+    stage.add(layer);
+
+    for (var i = 0; i < chickens.length; i++) {
+      chickens[i].start();
+    }
+
+    GreenDino.greenDinoObj.start();
+    keyBindings(GreenDino, GreenDino.greenDinoObj, 'up', 'left', 'right', 'space', '/');
+
+    requestAnimationFrame(gameLoop);
+
+  };
+
+  this.loadImages(this.sources, this.loadStage);
+
+  this.frames = 0;
+
+};
