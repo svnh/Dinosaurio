53a6ba9c8b6275231448ff7993809750bbcd92df
sekunovsky <sekunovsky@gmail.com>
2013-08-12 02:18:43 -0700

    added wat

--- game/public/src/init.js
+++ game/public/src/init.js
@@ -1,12 +1,22 @@
 var init = function() {
+  $('#titles').addClass('titles');
+  $('#titlecontent').addClass('titlecontent');
   $('.play').hide();
+  $('.wat').hide();
   $('.fork').hide();
   $('.keyboard').hide();
   $('.fight').hide();
-  $('#titles').addClass('titles')
-  $('#titlecontent').addClass('titlecontent')
   $('.waiting').show();
   $('.chickenCounter').show();
   $('.oppCounter').show();
   this.game = new Game();
-}
+};
+
+var wat = function() {
+  $('#titles').addClass('titles');
+  $('#titlecontent').addClass('titlecontent');
+  $('.fight').hide();
+  $('.wat').hide();
+  $('.play').css("top", "170px");
+  $('.play').css("left", "-390px");  
+};
